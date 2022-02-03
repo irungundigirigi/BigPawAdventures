24b3160108a4ff9b381949c70e6327d555a0785d
Irungu Ndigirigi <indigirigi@irosh.irosh>
2022-02-03 19:54:24 +0300

    carousel initial

--- pages/_app.js
+++ pages/_app.js
@@ -1,11 +1,29 @@
+import Head from 'next/head'
+import Script from 'next/script'
 import '../styles/globals.css'
 import Layout from '../components/layout'
 
 function MyApp({ Component, pageProps }) {
   return (
+    <>
+    <Head>
+      <link
+      rel="stylesheet"
+      href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
+      integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
+      crossorigin="anonymous"
+      />
+    </Head>
+  
+    <Script
+      src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
+      integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
+      crossorigin="anonymous"/>
+
     <Layout>
         <Component {...pageProps} />
     </Layout>
+    </>
     )
 }
 
--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,52 @@
 import Head from 'next/head'
 import Image from 'next/image'
+import Kelly from '../public/kelly_mtKenya.JPG'
+import t from '../public/2.JPG'
+import th from '../public/3.JPG'
+import Carousel from 'react-bootstrap/Carousel';
 
 
 export default function Home() {
   return (
     <div>
-      
+      <Carousel>
+  <Carousel.Item>
+    <Image
+      className="d-block w-100"
+      src={Kelly}
+      alt="First slide"
+    />
+    <Carousel.Caption>
+      <h3>First slide label</h3>
+      <p>Nulla vitae elit libero, a pharetra augue mollis interdum.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <Image
+      className="d-block w-100"
+      src={t} 
+      alt="Second slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Second slide label</h3>
+      <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+  <Carousel.Item>
+    <Image
+      className="d-block w-100"
+      src={th}
+      alt="Third slide"
+    />
+
+    <Carousel.Caption>
+      <h3>Third slide label</h3>
+      <p>Praesent commodo cursus magna, vel scelerisque nisl consectetur.</p>
+    </Carousel.Caption>
+  </Carousel.Item>
+</Carousel>
+    
     </div>
   )
 }
