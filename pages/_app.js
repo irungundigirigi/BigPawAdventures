4548c25f5ed5b57c5d801661688de83da18987ab
Irungu Ndigirigi <indigirigi@irosh.irosh>
2022-02-03 21:39:14 +0300

    carousel initial

--- pages/_app.js
+++ pages/_app.js
@@ -11,14 +11,14 @@ function MyApp({ Component, pageProps }) {
       rel="stylesheet"
       href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css"
       integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3"
-      crossorigin="anonymous"
+      crossOrigin="anonymous"
       />
     </Head>
   
     <Script
       src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/js/bootstrap.bundle.min.js"
       integrity="sha384-ygbV9kiqUc6oa4msXn9868pTtWMgiQaeYH7/t7LECLbyPA2x65Kgf80OJFdroafW"
-      crossorigin="anonymous"/>
+      crossOrigin="anonymous"/>
 
     <Layout>
         <Component {...pageProps} />
