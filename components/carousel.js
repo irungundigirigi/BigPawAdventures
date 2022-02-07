32baf666ad17c74db7c92b9125508b8bcf04c52f
Irungu Ndigirigi <indigirigi@irosh.irosh>
2022-02-07 18:08:12 +0300

    carousel update 2

--- components/carousel.js
+++ components/carousel.js
@@ -3,9 +3,9 @@ import AliceCarousel from 'react-alice-carousel';
 import Image from 'next/image';
 import "react-alice-carousel/lib/alice-carousel.css";
 import styles from  '../styles/carousel.module.css';
-import Image2 from '../public/2.JPG'
-import Image3 from '../public/3.JPG'
-import Image4 from '../public/4.JPG'
+import Image2 from '../public/2.JPG';
+import Image3 from '../public/3.JPG';
+import Image4 from '../public/4.JPG';
 
 
 
