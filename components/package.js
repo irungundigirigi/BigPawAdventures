1998f7a350c62b2b19f460e011ec150576ea90c6
Irungu Ndigirigi <indigirigi@irosh.irosh>
2022-02-09 09:40:07 +0300

    minor developments

---
+++ components/package.js
@@ -0,0 +1,31 @@
+import styles from '../styles/package.module.css';
+import Image from 'next/image'
+import zebra from '../public/2_days_maasai_mara.png';
+
+
+import React from 'react';
+
+export default function Package() {
+
+    
+  return (
+        <div className={styles.packageCard}>
+            <div className={styles.ImageContainer}>
+                <Image src={zebra} />
+            </div>
+            <div className={styles.cardHeader}>
+                2 Days Maasai Mara Safari
+            </div>
+            <div className={styles.content}>
+            This is an exciting 2 days  safari into the famous Maasai Mara,
+            the jewel of Kenya's wildlife and the greatest spot for wildebeest migration.
+            
+
+
+
+            </div>
+
+
+        </div>
+  );
+}
