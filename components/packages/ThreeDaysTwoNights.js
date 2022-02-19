33e1cb995503fcff966b9c7e8fed1cf3b1f41432
Irungu Ndigirigi <indigirigi@irosh.irosh>
2022-02-19 22:53:14 +0300

    minor changes

--- components/packages/ThreeDaysTwoNights.js
+++ components/packages/ThreeDaysTwoNights.js
@@ -16,7 +16,9 @@ export default function TreeDaysTwoNights () {
             <div className={styles.content}>
             This is a short safari featuring Maasai Mara National Reserve. The safari departs Nairobi in the morning
                 and involves a drive down to the the floor of the Rift Valley, making a stop at view point to see the
-                escarpment and the awesome sight of the valley and the features there in. To arrive Maasai Mara in timefor lunch, followed by a late afternoon game drive that introduces you to Kenya's most popular game
+                escarpment and the awesome sight of the valley and the features there in. 
+                To arrive Maasai Mara in timefor lunch, followed by a late afternoon game drive that introduces you to 
+                Kenyas most popular game
                 sanctuary.
             </div>
             <div className={styles.priceTag}> <span className={styles.From}>From</span> $600pp</div>
